62bd2f17ff0790aae2a13f36f2d65f527bcc005a
fhluo <20843637+fhluo@users.noreply.github.com>
2022-11-10 01:10:00 +0800

    refactor: rename frontend to web

--- main.go
+++ main.go
@@ -4,20 +4,23 @@ import (
 	"embed"
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
-//go:embed all:frontend/dist
+//go:embed all:web/dist
 var assets embed.FS
 
 func main() {
 	app := NewApp()
 
 	err := wails.Run(&options.App{
-		Title:            "json2go",
-		Width:            1366,
-		Height:           768,
-		Assets:           assets,
+		Title:  "json2go",
+		Width:  1366,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
 		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
