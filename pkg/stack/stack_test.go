15fb0daa6ab0fb7f1a24a4a7977a06be4c21d452
fhluo <20843637+fhluo@users.noreply.github.com>
2022-11-16 18:32:31 +0800

    feat: add a new package stack

---
+++ pkg/stack/stack_test.go
@@ -0,0 +1,48 @@
+package stack
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func valid(s string) bool {
+	stack := New[rune]()
+
+	for _, c := range s {
+		switch c {
+		case '(', '[', '{', '<':
+			stack.Push(c)
+		case ')':
+			if stack.IsEmpty() || stack.Pop() != '(' {
+				return false
+			}
+		case ']':
+			if stack.IsEmpty() || stack.Pop() != '[' {
+				return false
+			}
+		case '}':
+			if stack.IsEmpty() || stack.Pop() != '{' {
+				return false
+			}
+		case '>':
+			if stack.IsEmpty() || stack.Pop() != '<' {
+				return false
+			}
+		}
+	}
+
+	return stack.IsEmpty()
+}
+
+func TestStack(t *testing.T) {
+	assert.True(t, valid(""))
+	assert.True(t, valid("( )"))
+	assert.True(t, valid("[( )]"))
+	assert.True(t, valid("[( )]{}"))
+	assert.True(t, valid("<[( )]{}>"))
+
+	assert.False(t, valid("("))
+	assert.False(t, valid("([)]"))
+	assert.False(t, valid("[{( )]"))
+	assert.False(t, valid("{[( )]}>"))
+}
