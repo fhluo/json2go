a0d2e1b34b50c05fe01e95c75d64e46c3d17496e
fhluo <20843637+fhluo@users.noreply.github.com>
2022-12-01 20:30:03 +0800

    feat: use fluent-svelte

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
     import {Generate, ReadClipboard, WriteClipboard} from '../wailsjs/go/main/App.js'
     import Editor from "./Editor.svelte";
-    import Button from "./Button.svelte";
+    import "fluent-svelte/theme.css";
+    import {Button, MenuBar, MenuBarItem, MenuFlyoutDivider, MenuFlyoutItem, TextBlock, TextBox} from "fluent-svelte";
     import "./i18n"
     import {_, locale, locales} from "svelte-i18n";
+    import {EventsEmit} from "../wailsjs/runtime";
 
 
     let input: { text: string }
@@ -28,43 +30,64 @@
         WriteClipboard(output.text)
     }
 
+    let items = $locales.map(function (value) {
+        return {
+            name: value,
+            value: value,
+        }
+    })
+
+
 </script>
 
-<main class="flex flex-col w-screen h-screen px-6 py-4 space-y-4">
-    <div class="columns-2 gap-6 mt-4 h-full max-h-full mx-2">
-        <div class="code h-full max-h-full flex flex-col overflow-clip">
-            <div class="w-full bg-white/50 border-b border-t flex flex-row">
-                <span class="py-1 px-4 select-none text-yellow-600 font-mono">JSON</span>
-                <button on:click={pasteJSON}>{$_('paste', {default: 'Paste'})}</button>
-            </div>
-            <Editor class="w-full h-full border-b" lang="json" bind:editor={input}/>
-        </div>
-        <div class="code h-full max-h-full flex flex-col overflow-clip">
-            <div class="w-full bg-white/50 border-b border-t flex flex-row">
-                <span class="py-1 px-4 select-none text-purple-600 font-mono">Go</span>
-                <button on:click={copyCode}>{$_('copy', {default: 'Copy'})}</button>
-            </div>
-            <Editor class="w-full h-full border-b" lang="go" bind:editor={output}/>
-        </div>
-    </div>
+<main class="w-screen h-screen flex flex-col">
+    <MenuBar class="border-b">
+        <MenuBarItem>
+            {$_('File')}
+            <svelte:fragment slot="flyout">
+                <MenuFlyoutDivider/>
+                <MenuFlyoutItem>{$_('Settings')}</MenuFlyoutItem>
+                <MenuFlyoutDivider/>
+                <MenuFlyoutItem on:click={()=> EventsEmit("exit")}>{$_('Exit')}</MenuFlyoutItem>
+            </svelte:fragment>
+        </MenuBarItem>
+        <MenuBarItem>{$_('Edit')}</MenuBarItem>
+        <MenuBarItem>
+            {$_('Language')}
+            <svelte:fragment slot="flyout">
+                {#each $locales as _locale}
+                    <MenuFlyoutItem variant="radio" bind:group={$locale} value={_locale}>{$_(_locale)}</MenuFlyoutItem>
+                {/each}
+            </svelte:fragment>
+        </MenuBarItem>
+    </MenuBar>
 
-    <div class="flex flex-col space-y-2 mx-2 border rounded-lg px-4 py-3 bg-white/50">
-        <div class="flex flex-row items-center gap-x-5">
-            <div class="space-x-1.5">
-                <label>{$_('language', {default: 'Language'})}</label>
-                <select bind:value={$locale} class="w-fit py-1 border-gray-300 focus:ring-1 focus:ring-cyan-300 rounded">
-                    {#each $locales as locale}
-                        <option value={locale}>{locale}</option>
-                    {/each}
-                </select>
+    <div class="flex flex-col w-full h-full px-6 py-4 space-y-4 bg-white/50">
+        <div class="columns-2 gap-6 mt-4 h-full max-h-full mx-2">
+            <div class="code h-full max-h-full flex flex-col overflow-clip">
+                <div class="w-full bg-white/50 border-b border-t flex flex-row">
+                    <span class="py-1 px-4 select-none text-yellow-600 font-mono">JSON</span>
+                    <button on:click={pasteJSON} class="text-sm">{$_('Paste')}</button>
+                </div>
+                <Editor class="w-full h-full border-b" lang="json" bind:editor={input}/>
             </div>
-            <div class="space-x-1.5 flex-grow flex flex-row items-center">
-                <label for="allCaps" class="select-none">{$_('allCaps', {default: 'All caps'})}</label>
-                <input type="text" id="allCaps"
-                       class="rounded-md flex-grow border-gray-300 focus:ring-1 focus:ring-cyan-300 py-1 transition"
-                       bind:value={allCaps}>
+            <div class="code h-full max-h-full flex flex-col overflow-clip">
+                <div class="w-full bg-white/50 border-b border-t flex flex-row">
+                    <span class="py-1 px-4 select-none text-purple-600 font-mono">Go</span>
+                    <button on:click={copyCode} class="text-sm">{$_('Copy')}</button>
+                </div>
+                <Editor class="w-full h-full border-b" lang="go" bind:editor={output}/>
             </div>
-            <Button on:click={generate}>{$_('generate', {default: 'Generate'})}</Button>
+        </div>
+
+        <div class="flex flex-col space-y-2 mx-2 border rounded-lg px-4 py-3 bg-white/50">
+            <div class="flex flex-row items-center gap-x-5">
+                <div class="space-x-1.5 flex-grow flex flex-row items-center">
+                    <TextBlock class="select-none">{$_('All caps')}</TextBlock>
+                    <TextBox id="allCaps" class="flex-grow" bind:value={allCaps}></TextBox>
+                </div>
+                <Button variant="accent" on:click={generate}>{$_('Generate')}</Button>
+            </div>
         </div>
     </div>
 </main>
