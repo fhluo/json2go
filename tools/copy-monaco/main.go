7713317c8fb763e3ceda524afd6d9d9435fe07e9
fhluo <20843637+fhluo@users.noreply.github.com>
2023-02-09 00:24:58 +0800

    refactor: add function Copy

--- tools/copy-monaco/main.go
+++ tools/copy-monaco/main.go
@@ -13,6 +13,15 @@ func init() {
 	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stderr)))
 }
 
+func Copy(src string, dst string) {
+	if err := cp.Copy(src, dst); err != nil {
+		slog.Error("failed to copy", err, "src", src, "dst", dst)
+		os.Exit(1)
+	} else {
+		slog.Info("copied", "src", src, "dst", dst)
+	}
+}
+
 func main() {
 	wd, err := os.Getwd()
 	if err != nil {
@@ -35,43 +44,9 @@ func main() {
 
 	_ = os.MkdirAll(dst, 0660)
 
-	// copy src/base to dst/base
-	if err = cp.Copy(filepath.Join(src, "base"), filepath.Join(dst, "base")); err != nil {
-		slog.Error("failed to copy base", err)
-		os.Exit(1)
-	} else {
-		slog.Info("copied base", "src", filepath.Join(src, "base"), "dst", filepath.Join(dst, "base"))
-	}
-
-	// copy src/basic-languages/go to dst/basic-languages/go
-	if err = cp.Copy(filepath.Join(src, "basic-languages", "go"), filepath.Join(dst, "basic-languages", "go")); err != nil {
-		slog.Error("failed to copy basic-languages/go", err)
-		os.Exit(1)
-	} else {
-		slog.Info("copied basic-languages/go", "src", filepath.Join(src, "basic-languages", "go"), "dst", filepath.Join(dst, "basic-languages", "go"))
-	}
-
-	// copy src/editor to dst/editor
-	if err = cp.Copy(filepath.Join(src, "editor"), filepath.Join(dst, "editor")); err != nil {
-		slog.Error("failed to copy editor", err)
-		os.Exit(1)
-	} else {
-		slog.Info("copied editor", "src", filepath.Join(src, "editor"), "dst", filepath.Join(dst, "editor"))
-	}
-
-	// copy src/language/json to dst/language/json
-	if err = cp.Copy(filepath.Join(src, "language", "json"), filepath.Join(dst, "language", "json")); err != nil {
-		slog.Error("failed to copy language/json", err)
-		os.Exit(1)
-	} else {
-		slog.Info("copied language/json", "src", filepath.Join(src, "language", "json"), "dst", filepath.Join(dst, "language", "json"))
-	}
-
-	// copy src/loader.js to dst/loader.js
-	if err = cp.Copy(filepath.Join(src, "loader.js"), filepath.Join(dst, "loader.js")); err != nil {
-		slog.Error("failed to copy loader.js", err)
-		os.Exit(1)
-	} else {
-		slog.Info("copied loader.js", "src", filepath.Join(src, "loader.js"), "dst", filepath.Join(dst, "loader.js"))
-	}
+	Copy(filepath.Join(src, "base"), filepath.Join(dst, "base"))
+	Copy(filepath.Join(src, "basic-languages", "go"), filepath.Join(dst, "basic-languages", "go"))
+	Copy(filepath.Join(src, "editor"), filepath.Join(dst, "editor"))
+	Copy(filepath.Join(src, "language", "json"), filepath.Join(dst, "language", "json"))
+	Copy(filepath.Join(src, "loader.js"), filepath.Join(dst, "loader.js"))
 }
