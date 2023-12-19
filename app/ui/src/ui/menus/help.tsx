082b32970da37223f18e9194fa615956a907518c
fhluo <20843637+fhluo@users.noreply.github.com>
2023-12-19 21:00:03 +0800

    feat: add help menu

---
+++ app/ui/src/ui/menus/help.tsx
@@ -0,0 +1,34 @@
+import {MenubarContent, MenubarItem, MenubarMenu, MenubarSeparator, MenubarTrigger} from "@/components/ui/menubar.tsx"
+import {DialogTrigger} from "@/components/ui/dialog.tsx"
+import Updates from "@/ui/dialogs/updates.tsx"
+import About from "@/ui/dialogs/about.tsx"
+import {useTranslation} from "react-i18next"
+
+interface Props {
+    setDialog: (element: JSX.Element) => void
+}
+
+export default function ({setDialog}: Props) {
+    const {t} = useTranslation()
+
+    return (
+        <MenubarMenu>
+            <MenubarTrigger>
+                {t("help.title", "Help")}
+            </MenubarTrigger>
+            <MenubarContent>
+                <DialogTrigger asChild>
+                    <MenubarItem onClick={() => setDialog(<Updates/>)}>
+                        {t("help.updates", "Check for updates")}
+                    </MenubarItem>
+                </DialogTrigger>
+                <MenubarSeparator/>
+                <DialogTrigger asChild>
+                    <MenubarItem onClick={() => setDialog(<About/>)}>
+                        {t("help.about", "About")}
+                    </MenubarItem>
+                </DialogTrigger>
+            </MenubarContent>
+        </MenubarMenu>
+    )
+}
