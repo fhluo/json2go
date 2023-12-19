5ece71c18cc9dc9fcbd3912d72f5afe6004c9f0a
fhluo <20843637+fhluo@users.noreply.github.com>
2023-12-20 07:25:42 +0800

    feat: add examples menu

---
+++ app/ui/src/ui/menus/examples.tsx
@@ -0,0 +1,34 @@
+import {MenubarContent, MenubarItem, MenubarMenu, MenubarTrigger} from "@/components/ui/menubar.tsx"
+import {useEffect, useState} from "react"
+import {examples} from "../../../wailsjs/go/models.ts"
+import {GetExamples} from "../../../wailsjs/go/main/App"
+import {useTranslation} from "react-i18next"
+import Example = examples.Example
+
+interface Props {
+    replaceContent: (content: string) => void
+}
+
+export default function ({replaceContent}: Props) {
+    const {t} = useTranslation()
+
+    let [exampleList, setExampleList] = useState([] as Example[])
+    useEffect(() => {
+        GetExamples().then(value => {
+            setExampleList(value)
+        })
+    }, [])
+
+    return (
+        <MenubarMenu>
+            <MenubarTrigger>{t("examples.title", "Examples")}</MenubarTrigger>
+            <MenubarContent>
+                {exampleList.map(example =>
+                    <MenubarItem onClick={() => replaceContent(example.content)}>
+                        {example.title}
+                    </MenubarItem>
+                )}
+            </MenubarContent>
+        </MenubarMenu>
+    )
+}
