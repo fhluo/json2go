3ae9d8a2e10e1de87b6e7cda66d42e4f30d7ae30
fhluo <20843637+fhluo@users.noreply.github.com>
2023-12-20 15:22:07 +0800

    feat: add json editor

---
+++ app/ui/src/ui/json.tsx
@@ -0,0 +1,27 @@
+import {Layout, View} from "@/lib/types.ts"
+import {cn} from "@/lib/utils.ts"
+import {useTranslation} from "react-i18next"
+
+interface Props {
+    view: View
+    layout: Layout
+    pasteJSON: () => void
+}
+
+export default function ({view, layout, pasteJSON}: Props) {
+    const {t} = useTranslation()
+
+    return (
+        <div id="container-json" style={{display: view === View.GoOnly ? "none" : ""}}
+             className={cn({
+                 "col-span-2": layout === Layout.TwoColumns && view === View.JSONOnly,
+                 "row-span-2": layout === Layout.TwoRows && view === View.JSONOnly,
+             })}>
+            <div className="w-full bg-white/50 flex flex-row">
+                <span className="py-1 px-4 select-none text-yellow-700 font-mono">JSON</span>
+                <button onClick={pasteJSON}>{t("Paste")}</button>
+            </div>
+            <div className="w-full h-32 grow" id="json-editor"></div>
+        </div>
+    )
+}
