58b2235b44b19e9802414cd849f00a385fa94c12
fhluo <20843637+fhluo@users.noreply.github.com>
2024-09-25 17:38:42 +0800

    chore: bump tailwindcss version to 4.0

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   appType: "mpa",
-  plugins: [svelte()],
+  plugins: [tailwindcss(), svelte()],
   resolve: {
     alias: {
       "$lib": path.resolve("./src/lib"),
     },
   },
   build: {
-    outDir: "../json2go-wails/web/dist"
+    outDir: "../app/web/dist"
   }
 })
