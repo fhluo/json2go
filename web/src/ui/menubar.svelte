d59dfac33fc84f87982925a4b473ebbb1cfd7472
fhluo <20843637+fhluo@users.noreply.github.com>
2024-09-26 16:26:42 +0800

    refactor: rewrite menubar in svelte

---
+++ web/src/ui/menubar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { getContext } from "svelte";
+    import { Dialog } from "bits-ui";
+    import { Menubar } from "bits-ui";
+    import type { DialogState } from "../state/dialog.svelte";
+
+    const dialog = getContext<DialogState>("dialog");
+</script>
+
+<Dialog>
+    <Menubar class="rounded-none border-none bg-transparent">
+        <slot></slot>
+    </Menubar>
+    {#if dialog}
+        {dialog}
+    {/if}
+</Dialog>
