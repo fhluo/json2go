aa5fe5b10eef5cef61c65df2b87985b35c1b65a9
fhluo <20843637+fhluo@users.noreply.github.com>
2024-09-26 17:09:52 +0800

    fix: update path

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,9 +1,9 @@
-import { mount } from "svelte";
-import "../../json2go-web2/src/app.css";
+import {mount} from "svelte";
+import "./app.css";
 import App from "./app.svelte";
 
 const app = mount(App, {
-	target: document.getElementById("app")!,
+    target: document.getElementById("app")!,
 });
 
 export default app;
