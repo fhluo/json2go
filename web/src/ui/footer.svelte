01849ba3322768e2251242d3d4d56f1d9ffe98e2
fhluo <20843637+fhluo@users.noreply.github.com>
2024-09-26 19:06:51 +0800

    fix: correct state name

--- web/src/ui/footer.svelte
+++ web/src/ui/footer.svelte
@@ -1,22 +1,22 @@
 <script lang="ts">
-    import { getContext } from "svelte";
-    import { _ } from "svelte-i18n";
-    import { Button } from "bits-ui";
+    import {getContext} from "svelte";
+    import {_} from "svelte-i18n";
+    import {Button} from "bits-ui";
     import Message from "./message.svelte";
-    import type { EditorsState } from "../state/editors.svelte";
-    import type { MessageState } from "../state/message.svelte";
+    import type {EditorsState} from "../state/editors.svelte";
+    import type {MessageState} from "../state/message.svelte";
 
     const editors = getContext<EditorsState>("editors");
-    const messages = getContext<MessageState>("messages");
+    const message = getContext<MessageState>("message");
 
     function handleClick() {
-        messages.clear();
+        message.clear();
         editors.json2go();
     }
 </script>
 
 <div class="flex flex-row px-4 py-2 justify-end items-center h-12">
-    <Message />
+    <Message/>
     <Button size="sm" on:click={handleClick} class="mr-2">
         {$_("Generate")}
     </Button>
--- web/src/ui/message.svelte
+++ web/src/ui/message.svelte
@@ -3,17 +3,17 @@
     import { Cross1Icon, InfoCircledIcon } from "@radix-ui/react-icons";
     import type { MessageState } from "../state/message.svelte";
 
-    const messages = getContext<MessageState>("messages");
+    const message = getContext<MessageState>("message");
 
     function clearMessage() {
-        messages.clear();
+        message.clear();
     }
 </script>
 
-{#if messages.message}
+{#if message.message}
     <div class="flex flex-row items-center justify-center select-none mx-4 border bg-white/50 rounded shadow-sm space-x-1.5">
         <InfoCircledIcon class="w-5 h-5 ml-2 mr-1 text-red-600" />
-        <span>{messages.message}</span>
+        <span>{message.message}</span>
         <button type="button" on:click={clearMessage} class="hover:bg-gray-200/50 py-1 px-1 rounded-r transition flex items-center justify-center">
             <Cross1Icon class="w-5 h-5" />
         </button>
