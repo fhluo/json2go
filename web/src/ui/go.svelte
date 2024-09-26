fcace88856689102f5bbd89aca19cd926fbd8d4f
fhluo <20843637+fhluo@users.noreply.github.com>
2024-09-26 16:31:55 +0800

    refactor: rewrite go editor in svelte

---
+++ web/src/ui/go.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import { getContext } from "svelte";
+    import { _ } from "svelte-i18n";
+    import type { UIState } from "../state/ui.svelte";
+    import type { EditorsState } from "../state/editors.svelte";
+
+    const ui = getContext<UIState>("ui");
+    const editors = getContext<EditorsState>("editors");
+
+    function copyGo() {
+        editors.copyFromGoEditor();
+    }
+</script>
+
+<div
+    id="container-go"
+    style="display: {ui.view === 'JSON Only' ? 'none' : ''}"
+    class="group {ui.layout === 'Two Columns' && ui.view === 'Go Only' ? 'col-span-2' : ''} {ui.layout === 'Two Rows' && ui.view === 'Go Only' ? 'row-span-2' : ''} {ui.layout === 'Two Columns' && ui.view === 'JSON and Go' ? 'border-l' : ''} {ui.layout === 'Two Rows' && ui.view === 'JSON and Go' ? 'border-t' : ''}"
+>
+    <div class="w-full bg-white/50 flex flex-row">
+        <span class="py-1 px-4 select-none text-purple-700 font-mono group-focus-within:text-purple-500">
+            Go
+        </span>
+        <button type="button" on:click={copyGo}>
+            {$_("Copy")}
+        </button>
+    </div>
+    <div class="w-full h-32 grow" id="go-editor"></div>
+</div>
