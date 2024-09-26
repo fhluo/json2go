bcac5f8f85390fb18c6d54fc227c724709788e96
fhluo <20843637+fhluo@users.noreply.github.com>
2024-09-26 16:21:51 +0800

    refactor: rewrite container in svelte

---
+++ web/src/ui/container.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { getContext } from "svelte";
+    import { Layout } from "../state/ui.svelte";
+    import type { UIState } from "../state/ui.svelte";
+
+    const ui = getContext<UIState>("ui");
+</script>
+
+<div class={`grid h-64 grow border-t border-b ${ui.layout === Layout.TwoColumns ? 'grid-cols-2' : ''} ${ui.layout === Layout.TwoRows ? 'grid-rows-2' : ''}`}>
+    <slot></slot>
+</div>
