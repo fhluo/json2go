39228c10798c73f2130bfd8c6bb2ce0103894287
fhluo <20843637+fhluo@users.noreply.github.com>
2024-09-26 19:08:07 +0800

    refactor: use dialog state

--- web/src/ui/menus/help.svelte
+++ web/src/ui/menus/help.svelte
@@ -1,34 +1,34 @@
 <script lang="ts">
-    import { getContext } from "svelte";
-    import { _ } from "svelte-i18n";
-    import { Menubar, Dialog } from "bits-ui";
+    import {getContext} from "svelte";
+    import {_} from "svelte-i18n";
+    import {Menubar, Dialog} from "bits-ui";
     import About from "../dialogs/about.svelte";
     import Updates from "../dialogs/updates.svelte";
-    // import type { DialogState } from "../../store/dialog.svelte";
+    import type {DialogState} from "../../state/dialog.svelte";
 
-    // const dialog = getContext<DialogState>("dialog");
+    const dialog = getContext<DialogState>("dialog");
 
     function openAbout() {
-        // dialog.setDialog(About);
+        dialog.dialog = About;
     }
 
     function openUpdates() {
-        // dialog.setDialog(Updates);
+        dialog.dialog = Updates;
     }
 </script>
 
 <Menubar.Menu>
-    <Menubar.Trigger>{$_("help.title", { default: "Help" })}</Menubar.Trigger>
+    <Menubar.Trigger>{$_("help.title", {default: "Help"})}</Menubar.Trigger>
     <Menubar.Content>
         <Dialog.Trigger asChild>
             <Menubar.Item on:click={openUpdates}>
-                {$_("help.updates", { default: "Check for updates" })}
+                {$_("help.updates", {default: "Check for updates"})}
             </Menubar.Item>
         </Dialog.Trigger>
-        <Menubar.Separator />
+        <Menubar.Separator/>
         <Dialogs.Trigger asChild>
             <Menubar.Item on:click={openAbout}>
-                {$_("help.about", { default: "About" })}
+                {$_("help.about", {default: "About"})}
             </Menubar.Item>
         </Dialogs.Trigger>
     </Menubar.Content>
