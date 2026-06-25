2fc24493d76bfcfac94176115f24eaedac0e9e29
fhluo <20843637+fhluo@users.noreply.github.com>
2026-06-25 20:05:26 +0800

    chore: remove Svelte frontend

--- web/src/lib/i18n.ts
+++
@@ -1,9 +0,0 @@
-import { getLocaleFromNavigator, init, register } from "svelte-i18n";
-
-register("en", () => import("../../locales/en.json"));
-register("zh", () => import("../../locales/zh.json"));
-
-init({
-	fallbackLocale: "en",
-	initialLocale: getLocaleFromNavigator(),
-});
